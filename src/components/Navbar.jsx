d97663abea145508b1179ecff388630373b13dc5
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-01-13 20:03:17 +0500

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,6 +64,7 @@ export default function Navbar({ onNavigate }) {
             <div className="menu-box">
               {can("customer_ledger") && <a onClick={() => go("customerLedger")}>📒 Customer Ledger</a>}
               {can("purchase_ledger") && <a onClick={() => go("purchaseLedger")}>📦 Purchase Ledger</a>}
+              {can("supplier_ledger") && <a onClick={() => go("supplierLedger")}>📦 Supplier Ledger</a>}
               {can("bank_ledger") && <a onClick={() => go("bankLedger")}>🏦 Bank Ledger</a>}
               {can("cash_ledger") && <a onClick={() => go("cashLedger")}>💵 Cash Ledger</a>}
               {can("expense_ledger") && <a onClick={() => go("expenseLedger")}>💸 Expense Ledger</a>}
