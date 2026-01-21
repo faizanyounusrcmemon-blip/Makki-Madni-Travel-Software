b934606b5560fcb4a857251e134ac08fac1842f3
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-01-21 13:06:46 +0500

    Update Hotels.jsx

--- src/screens/Hotels.jsx
+++ src/screens/Hotels.jsx
@@ -179,7 +179,7 @@ export default function Hotels({ onNavigate }) {
         {/* Header */}
         <h3 style={styles.header}>✈️ MAKKI MADNI TRAVEL</h3>
         <div style={styles.contactInfo}>
-          Shop #4 Daimon City Building, Near Zeenat-ul-Islam Masjid<br />
+          Shop #4 Diamond City Building, Near Zeenat-ul-Islam Masjid<br />
           Garden West, Karachi<br />
           ✉️ makkimadnitravel@gmail.com | ☎️ 0335-7476744
         </div>
