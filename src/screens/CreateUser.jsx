3e38b5c757b26b03a1cc7f9f89384ae84a0790e3
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-03-18 11:11:38 +0500

    Update CreateUser.jsx

--- src/screens/CreateUser.jsx
+++ src/screens/CreateUser.jsx
@@ -328,14 +328,14 @@ export default function CreateUser({ onNavigate }) {
 
 <td>
   {u.last_login
-    ? new Date(new Date(u.last_login).getTime() + (19 * 60 * 60 * 1000))
+    ? new Date(new Date(u.last_login).getTime() + (0 * 60 * 60 * 1000))
         .toLocaleString()
     : "Never"}
 </td>
 
 <td>
   {u.last_logout
-    ? new Date(new Date(u.last_logout).getTime() + (19 * 60 * 60 * 1000))
+    ? new Date(new Date(u.last_logout).getTime() + (0 * 60 * 60 * 1000))
         .toLocaleString()
     : "Never"}
 </td>
