e972b6843b36ee6b0b2386295fa0100b5e4782ed
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-06-22 13:45:35 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,8 +28,8 @@ export default function Header({ title }) {
 
 <div
   style={{
-    width: 110,
-    height: 110,
+    width: 105,
+    height: 105,
     borderRadius: "50%",
     border: "2px solid #d4af37",
     overflow: "hidden",
@@ -44,8 +44,8 @@ export default function Header({ title }) {
     src="/logo.png"
     alt="Makki Madni Travel"
     style={{
-      width: "140%",
-      height: "140%",
+      width: "135%",
+      height: "135%",
       objectFit: "contain",
       transform: "scale(1.4)",
       display: "block",
