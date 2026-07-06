8f996328727dc35637a99b0f89ae712585e85276
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-07-06 16:38:52 +0500

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://makki-madni-backend.vercel.app",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
