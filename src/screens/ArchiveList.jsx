4ec935febc60f56ad3d4611154ad55cd3a8fb64d
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-07-06 16:27:32 +0500

    Update ArchiveList.jsx

--- src/screens/ArchiveList.jsx
+++ src/screens/ArchiveList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import API from "../api";
 import Swal from "sweetalert2";
 
 export default function ArchiveList({ onNavigate, onView, onLogs }) {
